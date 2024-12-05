f088a9979f1ba15ddadda5b1a25c842227d542d2
Студент МКАГ <student@mcag.college>
2024-12-05 11:19:16 +0300

    Ваше сообщение о коммите

---
+++ README.md
@@ -0,0 +1,6 @@
+# My Greeting Library
+## Установка
+'''bash
+pip install my_greeting_library
+from my_greeting_library import hllo
+print(hello("Мир"))
---
+++ setup.py
@@ -0,0 +1,15 @@
+'''python
+from setuptools import setup, find_packages
+setup(
+name='my_greeting_library'.
+version='0.1',
+packages=find_packages(),
+description='Простая библиотека для приветствий'
+author='Ваше имя',
+author_email='ваш_email@example.com',
+url='https://github.com/ваш_логин/my_greeting_library',
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+],
+)
